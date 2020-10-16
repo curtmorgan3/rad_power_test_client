1c099bea43c6fee15a8c35f73d9ed7ab72f4a3a7
Curt Morgan <curtmorgan3@gmail.com>
2020-10-15 17:10:17 -0700

    prod build

---
+++ build/precache-manifest.9d16c7ab82dd7263a8c6669721c8a979.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb599a22c2a178c6fca67a15483312d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e55960a7df107ec9fbdf",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f8b6336ef8ed5e95aab8",
+    "url": "/static/js/2.b1ceac20.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.b1ceac20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e55960a7df107ec9fbdf",
+    "url": "/static/js/main.86a632e7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
